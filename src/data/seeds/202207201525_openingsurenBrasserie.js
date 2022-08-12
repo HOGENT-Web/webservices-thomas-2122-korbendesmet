6d4debe388e3db56c829e529ca6c0c88ed845694
KorbenDeSmet <korbendesmet@hotmail.com>
2022-08-12 23:18:12 +0200

    Added dates

--- src/data/seeds/202207201525_openingsurenBrasserie.js
+++ src/data/seeds/202207201525_openingsurenBrasserie.js
@@ -6,7 +6,7 @@ module.exports = {
         await knex(tables.openingsurenBrasserie.tableName).insert([
             {
                 brasserieID: '1',
-                dag: 'vrijdag',
+                dag: 'maandag',
                 beginUur: '10:00:00.0000000',
                 eindUur: '20:00:00.0000000',
                 middagMaxAantal: '30',
@@ -14,9 +14,49 @@ module.exports = {
             },
             {
                 brasserieID: '2',
+                dag: 'dinsdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '3',
+                dag: 'woensdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '4',
+                dag: 'donderdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '5',
+                dag: 'vrijdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '6',
                 dag: 'zaterdag',
                 beginUur: '10:00:00.0000000',
                 eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '7',
+                dag: 'zondag',
+                beginUur: '00:00:00.0000000',
+                eindUur: '00:00:00.0000000',
                 middagMaxAantal: '30',
                 avondMaxAantal: '30'
             }
--- src/data/seeds/202207201530_openingsurenFoodtruck.js
+++ src/data/seeds/202207201530_openingsurenFoodtruck.js
@@ -5,18 +5,58 @@ module.exports = {
 
         await knex(tables.openingsurenFoodtruck.tableName).insert([
             {
-                foodtruckID: '1',
-                datum: '2022-08-15',
+                brasserieID: '1',
+                dag: 'maandag',
                 beginUur: '10:00:00.0000000',
                 eindUur: '20:00:00.0000000',
                 middagMaxAantal: '30',
                 avondMaxAantal: '30'
             },
             {
-                foodtruckID: '2',
-                datum: '2022-08-22',
+                brasserieID: '2',
+                dag: 'dinsdag',
                 beginUur: '10:00:00.0000000',
                 eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '3',
+                dag: 'woensdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '4',
+                dag: 'donderdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '5',
+                dag: 'vrijdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '6',
+                dag: 'zaterdag',
+                beginUur: '10:00:00.0000000',
+                eindUur: '20:00:00.0000000',
+                middagMaxAantal: '30',
+                avondMaxAantal: '30'
+            },
+            {
+                brasserieID: '7',
+                dag: 'zondag',
+                beginUur: '00:00:00.0000000',
+                eindUur: '00:00:00.0000000',
                 middagMaxAantal: '30',
                 avondMaxAantal: '30'
             }
